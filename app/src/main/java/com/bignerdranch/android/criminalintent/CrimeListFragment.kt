d829bba541c7e6d52a0266a1a4b5d0a57caf5105
josebgs <jsantos@riis.com>
2021-05-21 15:04:58 -0400

    implemented database functionality (chapter 11 content)

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -13,21 +13,18 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.text.format.DateFormat
+import androidx.lifecycle.Observer
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment: Fragment() {
 
     private lateinit var crimeRecyclerView: RecyclerView
-    private lateinit var adapter: CrimeAdapter
+    private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this@CrimeListFragment).get(CrimeListViewModel::class.java)
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,14 +36,25 @@ class CrimeListFragment: Fragment() {
         crimeRecyclerView =
             view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
+        crimeRecyclerView.adapter = adapter
 
-        updateUI()
         return view
 
     }
 
-    private fun updateUI(){
-        val crimes = crimeListViewModel.crimes
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        crimeListViewModel.crimeListLiveData.observe(
+            viewLifecycleOwner,
+            Observer { crimes ->
+                crimes?.let{
+                    Log.i(TAG, "Got crimes ${crimes.size}")
+                    updateUI(crimes)
+                }
+            })
+    }
+
+    private fun updateUI(crimes : List<Crime>){
         adapter = CrimeAdapter(crimes)
         crimeRecyclerView.adapter = adapter
     }
---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -0,0 +1,42 @@
+package com.bignerdranch.android.criminalintent
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.Room
+import database.CrimeDatabase
+import java.lang.IllegalStateException
+import java.util.*
+
+private const val DATABASE_NAME = "crime-database"
+private const val DATABASE_DIR = "databases/$DATABASE_NAME"
+
+class CrimeRepository private constructor(context: Context) {
+
+    private val database: CrimeDatabase = Room.databaseBuilder(
+        context.applicationContext,
+        CrimeDatabase::class.java,
+        DATABASE_NAME)
+        .createFromAsset(DATABASE_DIR)
+        .build()
+
+    private val crimeDao = database.crimeDao()
+
+    fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
+
+    fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+
+    companion object{
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context){
+            if(INSTANCE == null){
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+
+        fun get(): CrimeRepository{
+            return INSTANCE?:
+            throw IllegalStateException("CrimeRepository must be initialized")
+        }
+    }
+}
