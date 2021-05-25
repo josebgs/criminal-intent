847afdbd88ebd5153d2c2b9e8cd5f2fc6c9545e1
josebgs <jsantos@riis.com>
2021-05-25 16:10:43 -0400

    Added Chapter 12 Functionality

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.kt
@@ -1,5 +1,6 @@
 package com.bignerdranch.android.criminalintent
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -13,16 +14,33 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.text.format.DateFormat
+import java.util.*
 
 
 private const val TAG = "CrimeListFragment"
 class CrimeListFragment: Fragment() {
+
+
+    /**
+     * Required interface for hosting activities
+     */
+
+    interface Callbacks{
+        fun onCrimeSelected(crimeId: UUID)
+    }
+
+    private var callbacks: Callbacks? = null
 
     private lateinit var crimeRecyclerView: RecyclerView
     private var adapter: CrimeAdapter? = CrimeAdapter(emptyList())
 
     private val crimeListViewModel: CrimeListViewModel by lazy {
         ViewModelProvider(this@CrimeListFragment).get(CrimeListViewModel::class.java)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        callbacks = context as Callbacks?
     }
 
 
@@ -52,6 +70,11 @@ class CrimeListFragment: Fragment() {
                     updateUI(crimes)
                 }
             })
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callbacks = null
     }
 
     private fun updateUI(crimes : List<Crime>){
@@ -90,8 +113,7 @@ class CrimeListFragment: Fragment() {
         }
 
         override fun onClick(v: View){
-            Toast.makeText(context, "${crime.title} pressed!", Toast.LENGTH_SHORT)
-                .show()
+            callbacks?.onCrimeSelected(crime.id)
         }
     }
 
--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeRepository.kt
@@ -6,6 +6,8 @@ import androidx.room.Room
 import database.CrimeDatabase
 import java.lang.IllegalStateException
 import java.util.*
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 private const val DATABASE_NAME = "crime-database"
 private const val DATABASE_DIR = "databases/$DATABASE_NAME"
@@ -20,10 +22,23 @@ class CrimeRepository private constructor(context: Context) {
         .build()
 
     private val crimeDao = database.crimeDao()
+    private val executor = Executors.newSingleThreadExecutor()
 
     fun getCrimes(): LiveData<List<Crime>> = crimeDao.getCrimes()
 
     fun getCrime(id: UUID): LiveData<Crime?> = crimeDao.getCrime(id)
+
+    fun updateCrime(crime: Crime){
+        executor.execute{
+            crimeDao.updateCrime(crime)
+        }
+    }
+
+    fun addCrime(crime: Crime){
+        executor.execute{
+            crimeDao.addCrime(crime)
+        }
+    }
 
     companion object{
         private var INSTANCE: CrimeRepository? = null
